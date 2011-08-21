1e422b4a56927540571ec3d3f937925f3515f05d
berendjanwever <berendjanwever@gmail.com>
2011-08-21 20:48:43 +0000

    Initial version, based on modifications to source of Google code project w32-exec-calc-shellcode as suggested by Peter Ferrie.

---
+++ build_config.py
@@ -0,0 +1,39 @@
+build_config = {
+  "version": "0.1",
+  "projects": {
+    "w32-exec-calc-shellcode.bin": {
+      "architecture": "x86",
+      "files": {
+        "w32-exec-calc-shellcode.bin": {
+          "sources":  ["w32-exec-calc-shellcode.asm"],
+        },
+      },
+    },
+    "w32-exec-calc-shellcode-esp.bin": {
+      "architecture": "x86",
+      "files": {
+        "w32-exec-calc-shellcode-esp.bin": {
+          "sources":  ["w32-exec-calc-shellcode.asm"],
+          "defines":  {"STACK_ALIGN": "TRUE"},
+        },
+      },
+    },
+    "w64-exec-calc-shellcode.bin": {
+      "architecture": "x64",
+      "files": {
+        "w64-exec-calc-shellcode.bin": {
+          "sources":  ["w64-exec-calc-shellcode.asm"],
+        },
+      },
+    },
+    "w64-exec-calc-shellcode-esp.bin": {
+      "architecture": "x64",
+      "files": {
+        "w64-exec-calc-shellcode-esp.bin": {
+          "sources":  ["w64-exec-calc-shellcode.asm"],
+          "defines":  {"STACK_ALIGN": "TRUE"},
+        },
+      },
+    },
+  },
+}

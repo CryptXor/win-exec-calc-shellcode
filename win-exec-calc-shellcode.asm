d92d45373ecaa5a79caa21d4cdb41ffc4111bd9f
berendjanwever <berendjanwever@gmail.com>
2011-09-02 09:27:26 +0000

    Fix minor compilation issues in previous edit, add Peter Ferrie to Copyright holders everywhere.

--- win-exec-calc-shellcode.asm
+++ win-exec-calc-shellcode.asm
@@ -1,23 +1,18 @@
-; Copyright (c) 2009-2011, Berend-Jan "SkyLined" Wever <berendjanwever@gmail.com>
+; Copyright (c) 2009-2011, Berend-Jan "SkyLined" Wever <berendjanwever@gmail.com> and Peter Ferrie <peter.ferrie@gmail.com>
+; null-free x86/x64 branching code for calc.exe executing shellcode.
+; Works in any x86 or x64 application for Windows 5.0-7.0 all service packs.
 ; Project homepage: http://code.google.com/p/win-exec-calc-shellcode/
 ; All rights reserved. See COPYRIGHT.txt for details.
 BITS 32
-; null-free x86/x64 branching code for calc.exe executing shellcode.
-; (See http://skypher.com/wiki/index.php/Hacking/Shellcode).
-; This code is based on http://www.ragestorm.net/blogs/?p=376
 
 %ifdef STACK_ALIGN
     AND     SP, 0xFFFC
 %endif
-
-    ;no undocumented use of REX with potentially unpleasant future side-effects
-    XOR   ECX, ECX
-    XOR   RDX, R10
-    JECXZ w64_exec_calc_shellcode
-; In x64 opcodes, this translates to:
-;   XOR   ECX, ECX (and now RCX is zero)
-;   [REX prefix instruction in 64-bit, "DEC ECX" and "XOR EDX, EDX" in 32-bit]
-;   JECXZ w64_exec_calc_shellcode
+    ; x86                         ; x64
+    XOR   ECX, ECX                ; --->  XOR   ECX, ECX
+    DEC   ECX                     ; -,->  XOR   RDX, R10
+    XOR   EDX, EDX                ; /
+    JECXZ w64_exec_calc_shellcode ; 
 
 ; Because the stack has been aligned (if requested), this does not need to get
 ; done in the x86/x64 shellcodes.
